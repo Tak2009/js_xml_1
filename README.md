e7c0ed3ef4dcfb2ccee9b6ef9ad1e97f17d8f73b
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-04-27 22:43:46 +0100

    updated

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 
 https://github.com/Rob--W/cors-anywhere
+
+https://www.ipentec.com/document/javascript-xml-parsing
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function getData() {
+function getData1() {
     var xmlhttp = new XMLHttpRequest();
 
     xmlhttp.onreadystatechange = function () {
@@ -30,4 +30,38 @@ function getData() {
     }
     xmlhttp.open("GET", "sample.xml");
     xmlhttp.send();
+}
+
+function getData2() {
+  var xmlhttp = new XMLHttpRequest();
+
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4) {
+      if (xmlhttp.status == 200) {
+        var elem = document.getElementById("output");
+
+        elem.innerHTML += "----- getElementsByTagName -----<br/>";
+        var docelem = xmlhttp.responseXML.documentElement;
+        var nodes = docelem.getElementsByTagName("name");
+        for (i = 0; i < nodes.length; i++) {
+          elem.innerHTML += nodes[i].tagName + ":" + nodes[i].textContent + "<br/>";
+        }
+
+        var nodes = docelem.getElementsByTagName("height");
+        for (i = 0; i < nodes.length; i++) {
+          elem.innerHTML += nodes[i].tagName + ":" + nodes[i].textContent + "<br/>";
+        }
+
+        var nodes = docelem.getElementsByTagName("weight");
+        for (i = 0; i < nodes.length; i++) {
+          elem.innerHTML += nodes[i].tagName + ":" + nodes[i].textContent + "<br/>";
+        }
+        elem.innerHTML += "----------<br/>";
+      } else {
+        alert("status = " + xmlhttp.status);
+      }
+    }
   }
+  xmlhttp.open("GET", "sample.xml");
+  xmlhttp.send();
+}
