b186bbcd747df24ae211f4bd43d028af6cdb7da3
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-04-27 22:55:53 +0100

    changhed

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 https://github.com/Rob--W/cors-anywhere
 
 https://www.ipentec.com/document/javascript-xml-parsing
+
+
+readyState の値
+値	状態	説明
+0	UNSENT	クライアントが作成されました。(XMLHttpRequestが作成されました) open メソッドはまだ呼び出されていません。
+1	OPENED	open メソッドが呼び出されした。send メソッドが呼び出されていません。
+2	HEADERS	受信用の send メソッドが呼び出されました。ヘッダ値とステータスはまだ利用できない状態です。
+3	LOADING	ダウンロード中です。一部のデータを受信しました。 responseBody と responseText ステータスと応答ヘッダーは完全なデータではなく一部分となります。
+4	DONE	処理が完了しました。responseBody と responseText プロパティに完全なデータが保持されています。
--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 function getData1() {
+    // 下記のコードにより、XMLHttpRequestを利用して、sample.xmlファイルを取得
     var xmlhttp = new XMLHttpRequest();
-
+    // XMLHttpRequestが読み込みを完了すると、onreadystatechange イベントが発生します。XMLHttpRequestオブジェクトのreadyState の値を確認し、4であれば読み込み完了と判断します。さらにステータスコードが200であることを確認し、ドキュメントが正常に読み込まれたことを判定します。
     xmlhttp.onreadystatechange = function () {
       if (xmlhttp.readyState == 4) {
         if (xmlhttp.status == 200) {
-          var elem = document.getElementById("output");
 
-          elem.innerHTML += "----- getElementsByTagName -----<br/>";
-          var docelem = xmlhttp.responseXML.documentElement;
-          var nodes = docelem.getElementsByTagName("name");
+          // XMLが正常に取得できると、下記のコードを実行します。
+          var elem = document.getElementById("output");　
+　　　　　　elem.innerHTML += "----- getElementsByTagName -----<br/>";
+          var docelem = xmlhttp.responseXML.documentElement;　// 左のコードにより、読み込んだXMLのルートノードを取得できます。今回のXMLの場合は"items"ノードになります。
+　　　　　　var nodes = docelem.getElementsByTagName("name");
           for (i = 0; i < nodes.length; i++) {
             elem.innerHTML += nodes[i].tagName + ":" + nodes[i].textContent + "<br/>";
           }
@@ -28,6 +30,7 @@ function getData1() {
         }
       }
     }
+    // 下記のコードにより、XMLHttpRequestを利用して、sample.xmlファイルを取得
     xmlhttp.open("GET", "sample.xml");
     xmlhttp.send();
 }
@@ -42,19 +45,16 @@ function getData2() {
 
         elem.innerHTML += "----- getElementsByTagName -----<br/>";
         var docelem = xmlhttp.responseXML.documentElement;
-        var nodes = docelem.getElementsByTagName("name");
+        var nodes = docelem.getElementsByTagName("item");
         for (i = 0; i < nodes.length; i++) {
-          elem.innerHTML += nodes[i].tagName + ":" + nodes[i].textContent + "<br/>";
-        }
+          var namenodes = nodes[i].getElementsByTagName("name");
+          elem.innerHTML += namenodes[0].tagName + ":" + namenodes[0].textContent + "<br/>";
+          var heightnodes = nodes[i].getElementsByTagName("height");
+          elem.innerHTML += heightnodes[0].tagName + ":" + heightnodes[0].textContent + "<br/>";
+          var weightnodes = nodes[i].getElementsByTagName("weight");
+          elem.innerHTML += weightnodes[0].tagName + ":" + weightnodes[0].textContent + "<br/>";
 
-        var nodes = docelem.getElementsByTagName("height");
-        for (i = 0; i < nodes.length; i++) {
-          elem.innerHTML += nodes[i].tagName + ":" + nodes[i].textContent + "<br/>";
-        }
-
-        var nodes = docelem.getElementsByTagName("weight");
-        for (i = 0; i < nodes.length; i++) {
-          elem.innerHTML += nodes[i].tagName + ":" + nodes[i].textContent + "<br/>";
+          elem.innerHTML += "======<br/>";
         }
         elem.innerHTML += "----------<br/>";
       } else {
@@ -63,5 +63,6 @@ function getData2() {
     }
   }
   xmlhttp.open("GET", "sample.xml");
+  //xmlhttp.responseType = "document";
   xmlhttp.send();
 }
